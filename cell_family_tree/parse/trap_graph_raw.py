5f942584df990cfaeb2f5538a7e0b690d4d7ffe3
codydev901 <codydev901@gmail.com>
2021-07-04 16:07:23 -0600

    more RLS from area work, plotting enhancments

--- cell_family_tree/parse/trap_graph_raw.py
+++ cell_family_tree/parse/trap_graph_raw.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import os
 from scipy.signal import argrelextrema
+import plotly.graph_objs as go
 from .helpers import write_csv
 
 """
@@ -18,16 +19,21 @@ class TrapGraphRaw:
         self.trap_num = None
         self.graph = {}
         self.t_stop = self.df["time_num"].max()
+        self.stop_condition = "Full"
         self.time_sum_area = []
         self.time_sum_area_minima = []
         self.time_sum_area_maxima = []
+        self.time_num = []
         self.time_num_obj = []
         self.branch_nodes = []
+        self.num_divisions = 0
+        self.division_events = []
         self._on_init()
 
     def _on_init(self):
 
         starting_num_objs = self.df.query("time_num == 1")["total_objs"].tolist()[0]
+        self.time_num = self.df["time_num"].unique().tolist()
         if starting_num_objs != 1:
             raise ValueError("Multiple or No Objects at T1 - Not supported at the moment")
 
@@ -44,13 +50,16 @@ class TrapGraphRaw:
                 next_minima_index = i1
                 break
 
-        try:
-            max_area = max(self.time_sum_area[i:next_minima_index])
-        except ValueError:
-            max_area = None
+        max_area = 0
+        max_area_index = 0
+        for i1, a in enumerate(self.time_sum_area[i:next_minima_index]):
+            if a >= max_area:
+                max_area = a
+                max_area_index = i + i1
 
         return {"nlm_index": next_minima_index,
                 "nlm_max_area": max_area,
+                "nlm_max_area_index": max_area_index,
                 "nlm_distance": next_minima_index - i,
                 }
 
@@ -98,9 +107,10 @@ class TrapGraphRaw:
         # Track State
         is_dividing = False     # Mother cell in process of dividing cannot bud into another cell. Branching Locked.
         last_mc_node_name = None
+        last_dc_node_name = None
         division_start_index = None
         division_end_index = None
-        num_divisions = 0
+        self.num_divisions = 0
         time_since_last_division = 0
 
         # RLS Logic
@@ -108,100 +118,89 @@ class TrapGraphRaw:
 
             # Early Stop - No Cells Detected
             if self.time_num_obj[i] == 0:
+                self.stop_condition = "No Cells"
+                print("Break No Cells")
+                break
+
+            # Early Stop - Time Since Last Division
+            if time_since_last_division >= 5 and self.num_divisions != 0:
+                self.stop_condition = "No Divisions"
+                print("Break No Divisions")
                 break
 
             # End Division State if applicable
             if i == division_end_index:
                 is_dividing = False
-                num_divisions += 1
+                last_dc_node_name = None
+                self.num_divisions += 1
                 print("Division Complete")
-                print(division_start_index+1, division_end_index+1, num_divisions)
+                print(division_start_index, division_end_index, self.num_divisions)
 
             mc_node_name = "{}.1".format(t)
+            dc_node_name = "{}.2".format(t)
+
+            self.graph[mc_node_name] = []
+            if last_mc_node_name:
+                self.graph[mc_node_name].append(last_mc_node_name)
+                self.graph[last_mc_node_name].append(mc_node_name)
+            last_mc_node_name = mc_node_name
+            if i == division_start_index:
+                self.graph[mc_node_name].append(dc_node_name)
+                self.graph[dc_node_name] = [mc_node_name]
+                last_dc_node_name = dc_node_name
+            if is_dividing and i != division_start_index:
+                self.graph[dc_node_name] = [last_dc_node_name]
+                self.graph[last_dc_node_name].append(dc_node_name)
+                last_dc_node_name = dc_node_name
 
             current_area = self.time_sum_area[i]
             at_local_minima = self.time_sum_area_minima[i]
 
-            if at_local_minima:  # Reach Local Minima means we MIGHT have a branch start NEXT step
+            if at_local_minima and not is_dividing:  # Reach Local Minima means we MIGHT have a branch start NEXT step
 
                 nlmin_info = self._next_minima_helper(i)
 
                 if nlmin_info["nlm_max_area"] > current_area + same_cell_noise:
-                    division_start_index = i
+                    division_start_index = i+1
+                    time_since_last_division = 0
                     if nlmin_info["nlm_distance"] >= 5:
                         is_dividing = True
                         division_end_index = nlmin_info["nlm_index"]
+                        print("GOT DIV 5+", nlmin_info)
                     else:
                         is_dividing = True
                         nlmin_info = self._next_minima_helper(nlmin_info["nlm_index"])
                         division_end_index = nlmin_info["nlm_index"]
+                        print("GOT DIV RETRY", nlmin_info)
+                    # print("GOT DIVISION", division_start_index, division_end_index)
+                    self.division_events.append({"t_start": division_start_index+1,
+                                                 "t_stop": division_end_index,
+                                                 "previous_divisions": self.num_divisions,
+                                                 "length": division_end_index - division_start_index})
 
-            
+            if not is_dividing:
+                time_since_last_division += 1
+
+    def plot_rls_results(self):
+
+        fig = go.Figure()
+
+        fig.layout.title["text"] = "TrapNum:{} TStop:{} SumArea/TimeNum".format(self.trap_num,
+                                                                                self.t_stop)
+
+        ones = list(np.ones(len(self.time_num_obj)))
+
+        fig.add_trace(go.Scatter(x=self.time_num, y=self.time_sum_area, mode="lines+markers"))
+        fig.add_trace(go.Scatter(x=self.time_num, y=ones, text=self.time_num_obj, mode="text"))
+        for d_ev in self.division_events:
+            fig.add_vrect(x0=d_ev["t_start"],
+                          x1=d_ev["t_stop"],
+                          fillcolor="red",
+                          opacity=0.2,
+                          annotation_text=d_ev["previous_divisions"]+1)
 
 
-
-
-
-
-
-       #  sys.exit()
-
-        # Debug
-        # for i in range(len(self.time_sum_area)):
-        #     print(i+1, self.time_num_obj[i], self.time_sum_area[i], self.time_sum_area_minima[i], self.time_sum_area_maxima[i])
-
-
-
-        # avg_cell_area = sum([a/c for a, c in zip(self.time_sum_area, self.time_num_obj)]) / len(self.time_sum_area)
-        #
-        # last_mc_node_name = None
-        # branch_active = False
-        # branch_start_area = None
-        # last_area = self.time_sum_area[0]
-        #
-        # num_branches = 0
-        #
-        # cats = argrelextrema(np.array(self.time_sum_area), np.less)
-        #
-        # print(self.time_sum_area)
-        # print(cats)
-
-        # for i, t in enumerate(self.df["time_num"].unique()):
-        #
-        #     mc_node_name = "{}.1".format(t)
-        #
-        #     current_area = self.time_sum_area[i]
-        #     next_area = self.time_sum_area[i+1] if t != self.t_stop else self.time_sum_area[i]
-        #
-        #     if not branch_active:
-        #         if next_area - current_area >= 15:
-        #             print("Branch Starting: ", t)
-        #             branch_active = True
-        #             num_branches += 1
-        #     else:
-        #         dc_node_name = "{}.2".format(t)
-        #         if current_area - next_area >= 15:
-        #             print("Branch Ending: ", t)
-        #             branch_active = False
-        #
-        #     if not branch_active:
-        #         self.graph[mc_node_name] = []
-        #         if last_mc_node_name:
-        #             self.graph[mc_node_name].append(last_mc_node_name)
-        #             self.graph[last_mc_node_name].append(mc_node_name)
-        #         last_mc_node_name = mc_node_name
-        #
-        #     if self.time_num_obj[i] == 0:
-        #         break
-        #
-        #     if next_area - current_area > 100:
-        #         break
-        #
-        # print(self.graph)
-        # print(num_branches)
-        # print(self.time_sum_area)
-        # print(self.time_num_obj)
-
+        fig.show()
 
 
     def write_cytoscape_network_csv(self):
